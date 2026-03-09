503b7358850b433e8a3731a63fa22aa209f11edd
LUCAS SILVA DE BRITO <44275289889@info.local>
2026-03-09 19:52:31 -0300

    Pi brito

--- senaimath.py
+++ senaimath.py
@@ -14,3 +14,18 @@ valor_x = float(input("Digite o valor de x (maior que 0) para calcular o ln(x): 
 resultado = logefra(valor_x) # chama a funcao logefra para calcular o logaritmo natural do valor de x fornecido pelo usuário
 
 print(f"O valor aproximado de ln({valor_x}) é: {resultado}")
+
+
+
+def PiValue(x): #X =  A numero de termos, quanto mais tiver mais preciso o resultado é 
+    soma = 0 #Valor de pi que vai ser acrescido depois 
+    
+    for i in range(x): #aqui ele soma ate o termo informado e acresce no soma 
+        soma += ((-1)**i) / (2*i + 1)  #aqui acontece a inversao dos sinais e a a soma do numero impar escolhido no input dos termos
+
+    return 4 * soma # Multiplica tudo por 4 igual na formula 
+
+
+ntermos = int(input("Insira o numero de termos que vc deseja: ")) #usuario esoclher o tanto de termos
+
+print(PiValue(ntermos)) #print valor de pi
