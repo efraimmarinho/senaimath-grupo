ea5d216b33ed4a24d3a7c34c7304e7f8f62be7ea
Isabella <isabellabbf10@gmail.com>
2026-03-11 13:24:50 -0300

    adicionei o calculo do seno

--- senaimath.py
+++ senaimath.py
@@ -99,3 +99,41 @@ resultado = exponencial(exp)
 # Mostra o resultado final
 print(resultado)
 print(PiValue(ntermos)) #print valor de pi
+
+
+print('----- Seno -----')
+
+def fatorial(n): #comeca com 1 pois e a base do fatorial
+    resultado = 1
+ 
+ 
+    for i in range(1, n + 1): #ele vai receber um bloco de codigo e vai contar de 1 ate n
+        
+        resultado *= i #ele vai multiplicar acumulando, por exemplo n = 4  | 1 x 1 x 2 x 3 x 4
+
+    return resultado
+    
+
+def seno(x, termos = 10): # definiu termos = 10 pois e um numero equilibrado para boa aproximacao
+    
+    soma = 0 #variavel para guardar o resultado
+
+    
+    for n in range (termos): #vai repetir quantas vezes o termos for chaamado
+        
+        expoente = 2 * n + 1 # vai gerar 1, 3, 4, 5, 7 | Pq e a formula do seno usa pra potencias impares
+
+        sinal = (-1) ** n #ele vai alternar o sinal | n = 0 + 1 n 1 - 1 | vai alternar entre + e -
+        
+        termo = sinal * (x ** expoente) / fatorial(expoente) #vai montar cada parte da formula
+                        
+        soma += termo #vai somar tudo
+        
+    return soma
+    
+x = float(input('Digite o valor de x (em radianos): '))
+termos = int(input('Digite a quantidade de termos da série: '))
+
+resultado = seno(x, termos)
+
+print("Seno aproximado:", resultado)
